82d9b0758744bf0b044e874c04d63cc16047bddd
abdallahbilal <abdallahbilal39@gmail.com>
2024-07-09 11:33:24 -0400

    changed up the design

--- app/src/main/java/ca/georgiancollege/assignment3/MainActivity.kt
+++ app/src/main/java/ca/georgiancollege/assignment3/MainActivity.kt
@@ -17,11 +17,11 @@ class MainActivity : AppCompatActivity() {
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
         val dailyTasks = arrayOf(
-            Tasks_data("House of the Dragon", "HBO",),
-            Tasks_data("Lord of the Rings", "Prime Video"),
-            Tasks_data("Andor", "Disney"),
-            Tasks_data("Severance", "AppleTv"),
-            Tasks_data("Star Trek: Strange New Worlds", "Paramount+")
+            Tasks_data("Task 1", "0000001"),
+            Tasks_data("Task 2", "0000002"),
+            Tasks_data("Task 3", "0000003"),
+            Tasks_data("Task 4", "0000004"),
+            Tasks_data("Task 5", "0000005")
         )
         val firstAdapter = FirstAdapter(dailyTasks)
 // Use view binding to replace findViewById or synthetic properties
