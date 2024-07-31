9f8af5aed169148a78053c3c265401b0d6f344f3
abdallahbilal <abdallahbilal39@gmail.com>
2024-07-31 15:59:13 -0400

    added taskViewHolder

---
+++ app/src/main/java/ca/georgiancollege/assignment3/TaskViewHolder.kt
@@ -0,0 +1,18 @@
+package ca.georgiancollege.assignment3
+
+
+import androidx.recyclerview.widget.RecyclerView
+import ca.georgiancollege.assignment3.databinding.TextRowItemBinding
+
+// TaskViewHolder is a ViewHolder for the RecyclerView that displays a single Task item.
+class TaskViewHolder(private val binding: TextRowItemBinding) : RecyclerView.ViewHolder(binding.root) {
+
+    // Binds the data from a Task object to the views in the ViewHolder.
+    fun bind(task: Task) {
+        // Set the title of the task to the TextView
+        binding.Task.text = task.tasks_Title
+        // Set the comments of the Task to the TextView
+        binding.comments.text = task.comments
+
+    }
+}
