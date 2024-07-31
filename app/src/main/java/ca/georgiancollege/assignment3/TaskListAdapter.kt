3dd65ae9d5ee92032fd64c7de2c11a9dc75e9352
abdallahbilal <abdallahbilal39@gmail.com>
2024-07-31 16:03:16 -0400

    added taskListAdapter

---
+++ app/src/main/java/ca/georgiancollege/assignment3/TaskListAdapter.kt
@@ -0,0 +1,32 @@
+package ca.georgiancollege.assignment3
+
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.ListAdapter
+import ca.georgiancollege.assignment3.databinding.TextRowItemBinding
+
+// TaskListAdapter is an adapter for the RecyclerView that displays a list of Task items.
+// It extends ListAdapter, which helps with efficiently updating the list using the TaskComparator.
+class TaskListAdapter(private val onItemClicked: (Task) -> Unit) :
+    ListAdapter<Task, TaskViewHolder>(TaskComparator()) {
+
+    // Called when the RecyclerView needs a new ViewHolder to represent an item.
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): TaskViewHolder {
+        // Inflate the item layout and create a binding object
+        val binding = TextRowItemBinding.inflate(LayoutInflater.from(parent.context), parent, false)
+        // Return a new ViewHolder instance with the binding object
+        return TaskViewHolder(binding)
+    }
+
+    // Called by the RecyclerView to display the data at the specified position.
+    override fun onBindViewHolder(holder: TaskViewHolder, position: Int) {
+        // Get the TVShow item at the given position
+        val current = getItem(position)
+        // Bind the TVShow item to the ViewHolder
+        holder.bind(current)
+        // Set a click listener on the itemView to handle item clicks
+        holder.itemView.setOnClickListener {
+            onItemClicked(current)
+        }
+    }
+}
